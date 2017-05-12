cbfa9447f9b655f8e66583237e51f40ca9bff860
Asim Aslam <asim@chuhnk.me>
2017-05-12 15:13:27 +0100

    add Fatal for convenience

--- log.go
+++ log.go
@@ -1,6 +1,8 @@
 package log
 
 import (
+	"os"
+
 	"github.com/go-log/log"
 	golog "github.com/go-log/log/log"
 )
@@ -18,6 +20,18 @@ func Log(v ...interface{}) {
 // Logf makes use of github.com/go-log/log.Logf
 func Logf(format string, v ...interface{}) {
 	logger.Logf(format, v...)
+}
+
+// Fatal logs with Log and then exits with os.Exit(1)
+func Fatal(v ...interface{}) {
+	Log(v...)
+	os.Exit(1)
+}
+
+// Fatalf logs with Logf and then exits with os.Exit(1)
+func Fatalf(format string, v ...interface{}) {
+	Logf(format, v...)
+	os.Exit(1)
 }
 
 // SetLogger sets the local logger
